a04681c6c3d75b2e14c5a590f482819cf92c61de
Grunewald1 <gabrielgrunewald13@gmail.com>
2022-10-19 17:09:47 -0300

    adicionando api secret

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,15 @@
-function tempo(request, response) {
+ async function tempo(request, response) {
     const dinamicDate = new Date();
 
+    const subscribersResponse = await fetch(" https://api.convertkit.com/v3/subscribers?api_secret=<your_secret_api_key>&from=2016-02-01&to=2015-02-28")
+    const subscribersResponseJson = await subscribersResponse.json(); 
+    const inscritos = subscribersResponseJson.total_subscribers;
+    
+    
     response.json({
         date: dinamicDate.toGTMString()
+        inscritos: inscritos
+
     })
 
 }
