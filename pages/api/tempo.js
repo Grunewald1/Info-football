85f2df29d729328e218a29aba5881ebc1656ac64
Grunewald1 <gabrielgrunewald13@gmail.com>
2022-10-19 16:22:21 -0300

    criação api de tempo

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,7 +2,7 @@ function tempo(request, response) {
     const dinamicDate = new Date();
 
     response.json({
-        date: dinamicDate.toDateString()
+        date: dinamicDate.toGTMString()
     })
 
 }
