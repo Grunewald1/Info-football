19598a2d665cc57ae3ee9d9729c138042fec8d2a
Grunewald1 <gabrielgrunewald13@gmail.com>
2022-10-19 17:56:42 -0300

    secret api

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,15 +1,16 @@
  async function tempo(request, response) {
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;        
     const dinamicDate = new Date();
 
-    const subscribersResponse = await fetch(" https://api.convertkit.com/v3/subscribers?api_secret=<your_secret_api_key>&from=2016-02-01&to=2015-02-28")
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`)
     const subscribersResponseJson = await subscribersResponse.json(); 
     const inscritos = subscribersResponseJson.total_subscribers;
     
     
     response.json({
-        date: dinamicDate.toGTMString()
-        inscritos: inscritos
-
+    date: dinamicDate.toGTMString()
+    
+     
     })
 
 }
